4f88d70144b543677dc84f6c03222b4046287506
Dhanush <dhanushkrish45@gmail.com>
2022-01-31 21:44:09 +0530

    Fullstack Training Day-1

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ProductService } from './product.service';
+import { Product } from './product';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Test-1';
+  currentItem = 'Television';
+
+  products: Product[] = [];
+  productService;
+
+  constructor(){
+    this.productService=new ProductService();
+  }
+
+  getProducts() {
+    
+    this.products=this.productService.getProducts();
+  }
+}
+
+
